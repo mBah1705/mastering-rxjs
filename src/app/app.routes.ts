775812e531ebe28dc78351cef030c01bc4f64cba
Mamadou Bah <42182390+mBah1705@users.noreply.github.com>
2026-08-30 00:25:40 +0200

    feat: add FocussableBlock component with focus management and routing (#4)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,10 @@ import { CarsAndTrucksComponent } from './cars-and-trucks/cars-and-trucks.compon
 
 export const routes: Routes = [
     { path: '', redirectTo: 'todo', pathMatch: 'full' },
+    { path: 'todo', loadComponent: () => import('./todo/todo.component').then(m => m.TodoComponent) },
     { path: 'cars-and-trucks', loadComponent: () => import('./cars-and-trucks/cars-and-trucks.component').then(m => m.CarsAndTrucksComponent) },
     { path: 'timer', loadComponent: () => import('./timer/timer.component').then(m => m.TimerComponent) },
-    { path: 'todo', loadComponent: () => import('./todo/todo.component').then(m => m.TodoComponent) },
+    { path : 'focussable-block', loadComponent: () => import('./focussable-block/focussable-block.component').then(m => m.FocussableBlockComponent) },
+
+    { path: '**', redirectTo: 'todo' }
 ];
