5a19fcb87ddb8869c9098aa48c98f67b82d855e1
Mamadou Bah <42182390+mBah1705@users.noreply.github.com>
2026-08-29 21:54:42 +0200

    feat: different components on different routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CarsAndTrucksComponent } from './cars-and-trucks/cars-and-trucks.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'todo', pathMatch: 'full' },
+    { path: 'cars-and-trucks', loadComponent: () => import('./cars-and-trucks/cars-and-trucks.component').then(m => m.CarsAndTrucksComponent) },
+    { path: 'timer', loadComponent: () => import('./timer/timer.component').then(m => m.TimerComponent) },
+    { path: 'todo', loadComponent: () => import('./todo/todo.component').then(m => m.TodoComponent) },
+];
